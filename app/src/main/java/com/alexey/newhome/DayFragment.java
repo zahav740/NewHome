349bb5137ba6740d9bd4ecad8a332b971a1dfe4d
zahav740 <zahav740@gmail.com>
2023-05-19 19:39:19 +0300

    пробная листание исправил ошибку

--- app/src/main/java/com/alexey/newhome/DayFragment.java
+++ app/src/main/java/com/alexey/newhome/DayFragment.java
@@ -15,7 +15,7 @@ public class DayFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_day1, container, false);
+        View view = inflater.inflate(R.layout.fragment_day, container, false);
         // Инициализация и отображение истории за день
         return view;
     }
